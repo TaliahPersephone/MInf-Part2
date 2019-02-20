3189484005f141e82ca4909bf3c8ecf172afeaf9
Taliah Horner <taliahhorner@protonmail.com>
2019-02-20 15:09:24 +0000

    Gee

--- new_seizure/code/new/video_processing/targets.py
+++ new_seizure/code/new/video_processing/targets.py
@@ -2,18 +2,21 @@ import csv
 import numpy as np
 import time
 import threading
+import queue
 
 path = '/home/taliah/Documents/Course/Project/new_seizure/'
-file_in = 'temporal_annotations/1439328827509_00000{}_AZ324hrsno5and8_1.csv'
-out =	'data/targets/{:06}_{:05}.csv' 
-full = 'video/targets/{:06}.csv'
+file_in = path + 'temporal_annotations/1439328827509_{:06}_AZ324hrsno5and8_1.csv'
+out =	path + 'data/targets/{:06}_{:05}.csv' 
+full = path + 'video/targets/{:06}.csv'
 
+q = queue.Queue()
 
 split_targets():
 
 
 make_targets():
-		
+	vid_num = q.get() 	
+	annotations = csv.DictReader(file_in.format(vid_num),delimiter=',')
 
 
 
